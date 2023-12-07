15f898684c8628fede74a67170a873731284bbf9
Marcos <mvtgomes.ege@uesc.br>
2023-12-07 14:58:06 -0300

    Implementado Tribunal

---
+++ Avaliação Individual-Marcos/Tribunal de Justiça/src/Tribunal.java
@@ -0,0 +1,18 @@
+package Avaliação Individual-Marcos.Tribunal de Justiça.src;
+
+public class Tribunal {
+    private List<Processo> processos;
+
+    // Construtor e outros métodos...
+
+    // Getter
+    public List<Processo> getProcessos() {
+        return processos;
+    }
+
+    // Setter
+    public void setProcessos(List<Processo> processos) {
+        this.processos = processos;
+    }
+}
+
