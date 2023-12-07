273e27bd2b7cb2ded8c209f03c71952cc014bcf8
Marcos <mvtgomes.ege@uesc.br>
2023-12-07 15:24:37 -0300

    Criado gabinete do juiz e plenaria

---
+++ Avaliação Individual-Marcos/Tribunal de Justiça/src/classes/GabineteJuiz.java
@@ -0,0 +1,11 @@
+package Avaliação Individual-Marcos.Tribunal de Justiça.src.classes;
+
+public class GabineteJuiz extends Setor:
+    privado Juiz juizResponsavel
+    privado List<Processo> processos
+
+    // Construtor e outros métodos...
+
+    // Método para o juiz analisar um processo
+    método void analisarProcesso(Processo processo):
+        // Lógica para analisar o processo
---
+++ Avaliação Individual-Marcos/Tribunal de Justiça/src/classes/Plenario.java
@@ -0,0 +1,10 @@
+package Avaliação Individual-Marcos.Tribunal de Justiça.src.classes;
+
+public classe Plenario extends Setor:
+    privado List<Processo> processos
+
+    // Construtor e outros métodos...
+
+    // Método para realizar o julgamento no plenário
+    método void julgarPlenario(Processo processo):
+        // Lógica para o julgamento no plenário
