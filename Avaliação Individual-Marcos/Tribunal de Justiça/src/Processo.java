032bf49d642ada690bd9ceb86942027107cb35c2
Marcos <mvtgomes.ege@uesc.br>
2023-12-07 14:57:14 -0300

    Implementado classe Processos

---
+++ Avaliação Individual-Marcos/Tribunal de Justiça/src/Processo.java
@@ -0,0 +1,17 @@
+package Avaliação Individual-Marcos.Tribunal de Justiça.src;
+
+public class Processo {
+    private String numero;
+    private Parte parteAutora;
+    private Parte parteRe;
+    private Advogado advogadoParteAutora;
+    private DefensorPublico defensorPublicoParteRe;
+    private Juiz relator;
+    private Juiz revisor;
+    private Juiz prolator;
+    private String status;
+
+    // Construtor e outros métodos...
+
+    // Getters e Setters para os atributos privados
+}
