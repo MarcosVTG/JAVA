62bdf755babbc00615674263bd81f383bbd2a7c5
Marcos <mvtgomes.ege@uesc.br>
2023-12-07 15:06:57 -0300

    Criado Vistas

---
+++ Avaliação Individual-Marcos/Tribunal de Justiça/src/classes/Vista.java
@@ -0,0 +1,28 @@
+package Avaliação Individual-Marcos.Tribunal de Justiça.src.classes;
+
+public class Vista {
+    private Processo processo;
+    private Juiz juizResponsavel;
+
+    // Construtor e outros métodos...
+
+    // Getter
+    public Processo getProcesso() {
+        return processo;
+    }
+
+    // Setter
+    public void setProcesso(Processo processo) {
+        this.processo = processo;
+    }
+
+    // Getter
+    public Juiz getJuizResponsavel() {
+        return juizResponsavel;
+    }
+
+    // Setter
+    public void setJuizResponsavel(Juiz juizResponsavel) {
+        this.juizResponsavel = juizResponsavel;
+    }
+}
