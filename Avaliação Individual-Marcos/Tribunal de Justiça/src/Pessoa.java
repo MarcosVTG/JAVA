12e51dbb681ec082506a5ecf25409061cbb90aad
Marcos <mvtgomes.ege@uesc.br>
2023-12-07 14:50:57 -0300

    Implementado classes iniciais

---
+++ Avaliação Individual-Marcos/Tribunal de Justiça/src/Pessoa.java
@@ -0,0 +1,18 @@
+package Avaliação Individual-Marcos.Tribunal de Justiça.src;
+
+public class Pessoa {
+        
+    private String nome;
+
+        public String getNome() {
+            return nome;
+        }
+        
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+    }
+    
+    
+}
