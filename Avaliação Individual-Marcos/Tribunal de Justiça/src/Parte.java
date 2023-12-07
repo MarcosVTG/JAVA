9e49817949b3dd927100f784b0f704a258279cbb
Marcos <mvtgomes.ege@uesc.br>
2023-12-07 14:53:15 -0300

    Implementado Classe Parte

---
+++ Avaliação Individual-Marcos/Tribunal de Justiça/src/Parte.java
@@ -0,0 +1,17 @@
+package Avaliação Individual-Marcos.Tribunal de Justiça.src;
+
+
+    public class Parte extends Pessoa {
+        private List<Pessoa> membros;
+
+        public List<Pessoa> getMembros() {
+            return membros;
+        }
+        
+        public void setMembros(List<Pessoa> membros) {
+            this.membros = membros;
+        }
+    }
+    
+    
+
